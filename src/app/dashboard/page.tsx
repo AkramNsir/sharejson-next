7f6f883f139aa435f804df4d921c373ba84c2286
Akram Nsir <akramnsir5@gmail.com>
2025-06-07 15:04:14 +0100

    First commit

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,38 @@
+import JsonEditor from "@/components/json-editor";
+import prisma from "@/lib/db";
+import { currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+
+export default async function Page() {
+    const user = await currentUser();
+  if (!user) {
+    return redirect('/');
+  }
+
+  const loggedInUser = await prisma.user.findUnique({
+    where: { clerkUserId: user.id },
+  });
+  if (!loggedInUser) {
+    await prisma.user.create({
+      data: {
+        clerkUserId: user.id,
+        name: `${user.firstName} ${user.lastName}`,
+        imageUrl: user.imageUrl,
+        email: user.emailAddresses[0].emailAddress,
+      },
+    });
+  }
+  return (
+    <div>
+      <div className="my-8 max-w-4xl m-auto px-4">
+        <h1 className="text-3xl font-bold">Dashboard</h1>
+        <p className="text-muted-foreground">
+          manage your data and share it with others.
+        </p>
+      </div>
+
+      <JsonEditor />
+    </div>
+  );
+};
+
